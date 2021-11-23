9f7f462f420fb3069f10dbd61a8ef40a5a390d6e
Griffin Berlstein <griffin@berlste.in>
2021-11-23 15:32:29 -0500

    define interpreter for routing progs

--- src/structures.rs
+++ src/structures.rs
@@ -15,13 +15,7 @@ pub struct MemoryBank {
 }
 
 pub enum RoutingProgram {
-    Switch(Vec<(Condition, RoutingProgram)>),
-    Conditional {
-        port_val: u64,
-        comparison_operator: ComparisonOperator,
-        then: Box<RoutingProgram>,
-        otherwise: Box<RoutingProgram>,
-    },
+    Switch(Vec<(Condition, RoutingProgram)>, Box<RoutingProgram>),
     Shift {
         amount: usize,
         direction: ShiftDirection,
@@ -54,4 +48,53 @@ pub enum ComparisonOperator {
 pub enum ShiftDirection {
     Left,
     Right,
+}
+
+impl ComparisonOperator {
+    pub fn eval(&self, left: &u64, right: &u64) -> bool {
+        match self {
+            ComparisonOperator::LessThan => left < right,
+            ComparisonOperator::Equal => left == right,
+            ComparisonOperator::GreaterThan => left > right,
+            ComparisonOperator::NotEqual => left != right,
+            ComparisonOperator::LessThanOrEqual => left <= right,
+            ComparisonOperator::GreaterThanOrEqual => left >= right,
+        }
+    }
+}
+
+impl Condition {
+    pub fn eval(&self, port_val: u64) -> bool {
+        match self {
+            Condition::ComparisonPortVal(val, op) => op.eval(&port_val, val),
+            Condition::ComparisonValPort(val, op) => op.eval(val, &port_val),
+            Condition::And(c1, c2) => c1.eval(port_val) && c2.eval(port_val),
+            Condition::Or(c1, c2) => c1.eval(port_val) || c2.eval(port_val),
+            Condition::Not(c1) => !c1.eval(port_val),
+        }
+    }
+}
+
+impl RoutingProgram {
+    pub fn eval(&self, port_val: u64) -> u64 {
+        match self {
+            RoutingProgram::Switch(vec, default) => {
+                for (cond, prog) in vec.iter() {
+                    if cond.eval(port_val) {
+                        return prog.eval(port_val);
+                    }
+                }
+                default.eval(port_val)
+            }
+            RoutingProgram::Shift { amount, direction } => match direction {
+                ShiftDirection::Left => port_val << amount,
+                ShiftDirection::Right => port_val >> amount,
+            },
+            RoutingProgram::Add(v) => (port_val + v),
+            RoutingProgram::SubPortVal(v) => (port_val - v),
+            RoutingProgram::SubValPort(v) => (v - port_val),
+            RoutingProgram::Sequence(s) => s.iter().fold(port_val, |acc, x| x.eval(acc)),
+            RoutingProgram::Constant(c) => *c,
+        }
+    }
 }
